835be245e7200c4a5c275d144e90f9b772d3b700
aasmah <aas.29980@gmail.com>
2021-11-05 19:22:50 -0500

    cleared old commits

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your Code Here
+let reps = [ 10, 13, 12 ]
+renderChart(reps)
+//let reps = []
+//let reps = []
+let newNumberOfReps = window.prompt('Enter your number of reps: ')
+//let reps = []
+//let newNumberOfReps = window.prompt('Enter your number of reps: ')
+reps.push(newNumberOfReps)
+renderChart(reps)
+let serializedReps = JSON.stringify(reps)
